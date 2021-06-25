0f039090fc56eca16de3d620baac6885b3570796
katechengdu <k1530220@gmail.com>
2021-06-25 19:24:34 +0800

    after having added Book model. before adding Token model. -- sorting out front end -- home page search to be sorted out.

--- original_text/views.py
+++ original_text/views.py
@@ -10,6 +10,7 @@ class BookView(ListView):
     model = Book
     ordering = ['pk']
 
+
 class SentenceView(ListView):
     template_name = 'original_text/book.html'
     context_object_name = 'results'
@@ -20,9 +21,11 @@ class SentenceView(ListView):
         book_title = Book.objects.get(id=self.kwargs.get('pk')).title
         datas = Sentence.objects.filter(chapter_title__book_title__id=self.kwargs.get('pk'))
         chapter_titles=Chapter.objects.filter(book_title__id=self.kwargs.get('pk'))
+        all_books=Book.objects.all()
         results = {'book_title':book_title,
                     'datas':datas,
                     'chapter_titles': chapter_titles,
+                    'all_books': all_books,
                     'text_language':['french','english']
                    }
         return results
@@ -36,16 +39,18 @@ class SearchView(ListView):
     @csrf_exempt
     def get_queryset(self,*args,**kwargs):
         q1 = self.request.GET.get("text_story")
-        q2 = self.request.GET.get("chapter_name")
+        q2 = self.request.GET.get("book_title")
         q3 = self.request.GET.get("text_language")
         chapter_titles = Chapter.objects.filter(book_title__id=self.kwargs.get('pk'))
+        all_books = Book.objects.all()
         if q1 or q2 or q3:
             if q2 == 'all' and q3 == 'french':
-                results_sentence = Sentence.objects.filter(chapter_title__book_title__id=self.kwargs.get('pk')).filter(content__icontains=q1).order_by('id')
+                results_sentence = Sentence.objects.filter(content__icontains=q1).order_by('id')
                 total_results_sentence = len(results_sentence)
                 results = {'results_sentence': results_sentence,
                            'total_results_sentence':total_results_sentence,
                            'chapter_titles':chapter_titles,
+                           'all_books':all_books,
                            'text_language':['french','english'],
                            'q1':q1,
                            'q2':q2,
@@ -53,11 +58,12 @@ class SearchView(ListView):
                            }
                 return results
             elif q2 == 'all' and q3 == 'english':
-                results_sentence = Sentence.objects.filter(chapter_title__book_title__id=self.kwargs.get('pk')).filter(translation_eng__icontains=q1).order_by('id')
+                results_sentence = Sentence.objects.filter(translation_eng__icontains=q1).order_by('id')
                 total_results_sentence = len(results_sentence)
                 results = {'results_sentence': results_sentence,
                            'total_results_sentence':total_results_sentence,
                            'chapter_titles':chapter_titles,
+                           'all_books': all_books,
                            'text_language':['french','english'],
                            'q1': q1,
                            'q2': q2,
@@ -65,11 +71,12 @@ class SearchView(ListView):
                            }
                 return results
             elif q3 == 'french':
-                results_sentence = Sentence.objects.filter(chapter_title__book_title__id=self.kwargs.get('pk')).filter(content__icontains=q1,chapter_title__title_name=q2).order_by('id')
+                results_sentence = Sentence.objects.filter(chapter_title__book_title__title=q2).filter(content__icontains=q1).order_by('id')
                 total_results_sentence = len(results_sentence)
                 results = {'results_sentence': results_sentence,
                            'total_results_sentence':total_results_sentence,
                            'chapter_titles':chapter_titles,
+                           'all_books': all_books,
                            'text_language':['french','english'],
                            'q1': q1,
                            'q2': q2,
@@ -77,11 +84,12 @@ class SearchView(ListView):
                            }
                 return results
             elif q3 == 'english':
-                results_sentence = Sentence.objects.filter(chapter_title__book_title__id=self.kwargs.get('pk')).filter(translation_eng__icontains=q1,chapter_title__title_name=q2).order_by('id')
+                results_sentence = Sentence.objects.filter(chapter_title__book_title__title=q2).filter(translation_eng__icontains=q1).order_by('id')
                 total_results_sentence = len(results_sentence)
                 results = {'results_sentence': results_sentence,
                            'total_results_sentence':total_results_sentence,
                            'chapter_titles':chapter_titles,
+                           'all_books': all_books,
                            'text_language':['french','english'],
                            'q1': q1,
                            'q2': q2,
