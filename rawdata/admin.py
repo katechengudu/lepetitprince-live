28d2049e619805732a89f6f9f6be42ae05c2fdfc
katechengdu <k1530220@gmail.com>
2021-07-10 19:14:26 +0800

    Rawdata models are now ready for using NLP.

--- rawdata/admin.py
+++ rawdata/admin.py
@@ -6,15 +6,42 @@ from import_export.widgets import ForeignKeyWidget
 
 # Register your models here.
 
-
+class SourceAdmin(ImportExportModelAdmin):
+    list_display = ['title','pk','note']
+    list_display_links = ['title']
+    search_fields = ['title']
+    class Meta:
+        model = Source
 
 class RawdataAdmin(ImportExportModelAdmin):
-    list_display = ['pk','content','note','priority','number','translation_eng']
-    list_display_links = ['content','number']
+    list_display = ['pk','content','note','priority','translation_eng']
+    list_display_links = ['content']
     search_fields = ['pk','content']
     list_filter = ['note','priority']
-
     class Meta:
         model = Rawdata
 
+
+
+class NLP_token_RawdataAdmin(ImportExportModelAdmin):
+    list_display = ['pk','note','nlp_text','nlp_pos']
+    list_display_links = ['nlp_text']
+    search_fields = ['pk','nlp_text']
+    list_filter = ['note','nlp_pos']
+    class Meta:
+        model = NLP_token_Rawdata
+
+
+class LemmaAdmin(ImportExportModelAdmin):
+    list_display = ['pk','text']
+    list_display_links = ['text']
+    search_fields = ['pk','text']
+    list_filter = ['note','priority']
+    class Meta:
+        model = Lemma
+
+
+admin.site.register(Source,SourceAdmin)
 admin.site.register(Rawdata,RawdataAdmin)
+admin.site.register(NLP_token_Rawdata,NLP_token_RawdataAdmin)
+admin.site.register(Lemma,LemmaAdmin)
--- rawdata/models.py
+++ rawdata/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+class Source(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    file = models.FileField(null=True,blank=True,upload_to='raw_data/source/')
+    note = models.CharField(max_length=100,null=True,blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
 class Rawdata(models.Model):
-    number = models.IntegerField(null=True, blank=True)
+    source_title = models.ForeignKey(Source,on_delete=models.CASCADE)
+    book_id = models.IntegerField(null=True, blank=True)
+    chapter_id = models.IntegerField(null=True, blank=True)
+    sentence_id = models.IntegerField(null=True, blank=True)
+    sentence_number = models.IntegerField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     content_lower_case = models.TextField(null=True, blank=True)
     translation_eng = models.TextField(null=True,blank=True)
     translation_chi = models.TextField(null=True, blank=True)
+    token_total_french = models.IntegerField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     note = models.CharField(max_length=500,default='none')
     priority = models.BooleanField(default=False)
@@ -16,5 +29,33 @@ class Rawdata(models.Model):
         return self.note
 
 
+class NLP_token_Rawdata(models.Model):
+    source_sentence = models.ForeignKey(Rawdata,on_delete=models.CASCADE)
+    book_id = models.IntegerField(null=True, blank=True)
+    chapter_id = models.IntegerField(null=True, blank=True)
+    sentence_id = models.IntegerField(null=True, blank=True)
+    sentence_number = models.IntegerField(null=True, blank=True)
+    sentence = models.CharField(max_length=2000,null=True,blank=True)
+    token_language = models.CharField(max_length=50, default='french')
+    nlp_text = models.CharField(max_length=800,null=True,blank=True)
+    nlp_lemma = models.CharField(max_length=800,null=True,blank=True)
+    nlp_pos = models.CharField(max_length=800,null=True,blank=True)
+    nlp_tag = models.CharField(max_length=800,null=True,blank=True)
+    nlp_dep = models.CharField(max_length=800, null=True, blank=True)
+    note = models.CharField(max_length=800,null=True,blank=True)
+    token_nunmber = models.IntegerField(null=True, blank=True)
+    vocab_frequency_score = models.IntegerField(null=True, blank=True)
+    notes_vocab_frequency_score = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nlp_text
 
 
+class Lemma(models.Model):
+    text = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    note = models.CharField(max_length=500,null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    priority = models.BooleanField(default=False)
+    finished = models.BooleanField(default=False)
+    def __str__(self):
+        return self.text
